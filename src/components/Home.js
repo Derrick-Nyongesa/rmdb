ee6d1666f6dfc34f59af238207adbef1b27a05a6
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-09-14 14:30:12 +0300

    Created Thumb component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
 
 
 
@@ -79,8 +80,18 @@ class Home extends Component {
           />
         ) : null}
         <Grid header='Popular Movies'>{movies.results.map(movie => (
-          <div key={movie.id}>{movie.title}</div>
-        ))}</Grid>
+          <Thumb
+          key={movie.id}
+          clickable
+          image={
+            movie.poster_path
+              ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+              : NoImage
+          }
+          movieId={movie.id}
+        />
+        ))}
+        </Grid>
         
         
       </>
