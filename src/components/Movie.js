1f9bc9dda4b1c84bbd03c85e8cab02b158fa670d
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-09-15 10:01:26 +0300

    Fetch movie data from API

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 //config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 //components
 import Grid from './Grid';
 import Spinner from './Spinner';
 //hooks
-
+import { useMovieFetch } from '../hooks/useMovieFetch';
 //image
 import NoImage from '../images/no_image.jpg';
 
 const Movie = () =>{
+    const {movieId} = useParams();
+    const { state: movie, loading, error} = useMovieFetch(movieId)
     return (
         <div>
             <div>Movie</div>
