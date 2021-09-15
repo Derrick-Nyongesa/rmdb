316e4a0a1a7df241a8b82581cc09f5583f97fd4a
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-09-15 09:40:52 +0300

    Created Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+//routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 //styles
 import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header></Header>
-      <Home></Home>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>;
+          <Route path='/:movieId' element={<Movie></Movie>}></Route>;
+          <Route path='/*' element={<NotFound></NotFound>}></Route>;
+        </Routes>
       <GlobalStyle></GlobalStyle>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+//components
+import Grid from './Grid';
+import Spinner from './Spinner';
+//hooks
+
+//image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () =>{
+    return (
+        <div>
+            <div>Movie</div>
+        </div>
+    )
+};
+
+export default Movie;
