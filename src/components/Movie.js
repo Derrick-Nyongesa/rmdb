c641d499ab071f7d0470d76c87042ebdb3d97fb3
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-09-15 12:18:10 +0300

    Created other components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,10 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 //components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
+import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 //hooks
 import { useMovieFetch } from '../hooks/useMovieFetch';
 //image
@@ -12,12 +16,27 @@ import NoImage from '../images/no_image.jpg';
 
 const Movie = () =>{
     const {movieId} = useParams();
-    const { state: movie, loading, error} = useMovieFetch(movieId)
+    const { state: movie, loading, error} = useMovieFetch(movieId);
+    if (loading) return <Spinner></Spinner>;
+    if (error) return <div>SOmething went wrong...</div>;
     return (
         <div>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie.original_title}></BreadCrumb>
+            <MovieInfo movie={movie}></MovieInfo>
+            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue}></MovieInfoBar>
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor 
+                    key={actor.credit_id} 
+                    name={actor.name} 
+                    character={actor.character}
+                    imageUrl={actor.profile_path 
+                        ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                        : NoImage}></Actor>
+                ))}
+            </Grid>
         </div>
-    )
+    );
 };
 
 export default Movie;
